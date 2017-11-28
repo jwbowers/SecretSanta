785a1253b41df4033daa0c8098f08db9a28a2115
Jake Bowers <jwbowers@users.noreply.github.com>
2017-11-27 21:21:53 -0600

    Update assignsecretsantas.R

--- assignsecretsantas.R
+++ assignsecretsantas.R
@@ -1,7 +1,17 @@
 
-set.seed(20151225)
-people<-c("Fair","Brent","Cara","Jake","Mari","Cosmo","Ben","Alex","Lisa","Andy","Cole")
-family<-c(1,1,2,2,2,2,3,3,4,4,4)
+##set.seed(20151225)
+##people<-c("Fair","Brent","Cara","Jake","Mari","Cosmo","Ben","Alex","Lisa","Andy","Cole")
+##family<-c(1,1,2,2,2,2,3,3,4,4,4)
+## Read in data from Google Sheets 
+library("googledrive")
+sheetnm <-  drive_get("~/SecretSanta2007"))
+dat <- drive_get(as_id(sheetnm$id))
+## Remove non-participants
+dat <- dat[dat$Participant=="Yes",]
+## Assign the vectors for family and people
+family <- dat$Family
+people <- dat$Name
+
 famdist<-outer(family,family,function(x,y){ x==y })
 dimnames(famdist)<-list(people,people)
 pdist<-outer(people,people,function(x,y){ paste(x," to ",y,sep="") })
@@ -16,7 +26,7 @@ for(i in 1:length(people)){
   thesource[thesource==thesamp]<-NA
 }
 
-write.csv(res,file="xmas2015.csv")
+write.csv(res,file="xmas2017.csv")
 
 
 
