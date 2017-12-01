285557c1f2c7b8f09458d863108d19feaadcada7
Jake Bowers <jake@jakebowers.org>
2017-11-30 22:57:45 -0600

    Version ready to go once I have one more last name

--- assignsecretsantas.R
+++ assignsecretsantas.R
@@ -1,33 +1,74 @@
+### This script randomly assigns people into pairs (one secret santa and one gift recipient) across families
+### It also sends an email to each santa informing them about their gift recipient.
 
-##set.seed(20151225)
-##people<-c("Fair","Brent","Cara","Jake","Mari","Cosmo","Ben","Alex","Lisa","Andy","Cole")
-##family<-c(1,1,2,2,2,2,3,3,4,4,4)
-## Read in data from Google Sheets 
+## Read in data from Google Sheets
+install.packages("googledrive")
 library("googledrive")
-#theid <- drive_find("SecretSanta2017")
-drive_download("SecretSanta2017",type="csv")
-dat <- read.csv("SecretSanta2017.csv")
+library(dplyr)
+library(purrr)
+library(readr)
+library(gmailr)
+
+drive_download("SecretSanta2017",type="csv",overwrite=TRUE)
+origdat <- read.csv("SecretSanta2017.csv",as.is=TRUE)
 ## Remove non-participants
-dat <- dat[dat$Participant=="Yes",]
-## Assign the vectors for family and people
-family <- dat$Family
-people <- dat$Name
+## the base R way: dat <- dat[dat$Participant=="Yes",]
+## The ddplyr way
+dat <- filter(origdat,Participant=="Yes")
 
-famdist<-outer(family,family,function(x,y){ x==y })
-dimnames(famdist)<-list(people,people)
-pdist<-outer(people,people,function(x,y){ paste(x," to ",y,sep="") })
-dimnames(pdist)<-list(from=people, to=people)
+# Using Mari's algorithm
+## Pull out vectors for family and people just to make the code easier to read below
+thefamily <- dat$Family
+uniqname <- dat$UniqName
 
-## A slow ugly greedy algorithmn
-thesource<-1:length(people)
-res<-vector(length=length(people))
-for(i in 1:length(people)){
-  thesamp<-sample(thesource[!famdist[i,] & !is.na(thesource)],1)
-  res[i]<-pdist[i,thesamp]
-  thesource[thesource==thesamp]<-NA
+santas <- vector(length=length(uniqname))
+## Start with the first person
+set.seed(12345)
+santas[1] <- sample( uniqname[ thefamily != thefamily[1] ],size=1 )
+for(s in 2:length(uniqname)){
+  santas[s] <- sample( uniqname[ ( thefamily != thefamily[s] ) & !(uniqname %in% santas) ], size=1 )
 }
 
-write.csv(res,file="xmas2017.csv")
+dat$santa <- santas
 
+thebody <- "Dear %s,
 
+You are the secret santa for %s. You can use the attached Google Sheet to learn more about your gift recipient (https://drive.google.com/open?id=1hM0HBpSu_KzOfF9kc75MvNvfgrVHnvngF1XJ_1Yg3T4).
 
+We will be exchanging our gifts on December 23 at Leah, Brock, Lucy, Tyler, and Elle's house.
+
+Love,
+
+Jake and Mari
+"
+
+#testdat <- filter(dat,Family=="BowersWong")
+#testdat <- droplevels(testdat[-3,])
+
+edat<-dat %>% mutate(To=sprintf('%s <%s>',UniqName,Email),
+                        From='Jake Bowers <jake@jakebowers.org>',
+                        Subject='Secret Santa Assignment',
+                        body=sprintf(body,UniqName,santa)) %>% select(To,From,Subject,body)
+
+write_csv(edat, "composed-emails.csv")
+emails <- edat %>% pmap(mime)
+str(emails, max.level = 2, list.len = 2)
+
+## This next line sends all of the emails.
+sent_mail <- emails %>% map(send_message)
+
+# famdist<-outer(family,family,function(x,y){ x==y })
+# dimnames(famdist)<-list(people,people)
+# pdist<-outer(people,people,function(x,y){ paste(x," to ",y,sep="") })
+# dimnames(pdist)<-list(from=people, to=people)
+#
+# ## A slow ugly greedy algorithmn
+# thesource<-1:length(people)
+# res<-vector(length=length(people))
+# for(i in 1:length(people)){
+#   thesamp<-sample(thesource[!famdist[i,] & !is.na(thesource)],1)
+#   res[i]<-pdist[i,thesamp]
+#   thesource[thesource==thesamp]<-NA
+# }
+#
+# write.csv(res,file="xmas2017.csv")
